c124464ded925b421904ec15bcf54b622cb7db05
PabloSan1997 <jos_pab_997@hotmali.com>
2024-03-24 19:41:24 -0600

    Transactional

--- src/main/java/com/pablo/springjpa/spirngbootjpa/SpirngbootJpaApplication.java
+++ src/main/java/com/pablo/springjpa/spirngbootjpa/SpirngbootJpaApplication.java
@@ -1,11 +1,14 @@
 package com.pablo.springjpa.spirngbootjpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pablo.springjpa.spirngbootjpa.entities.Person;
 import com.pablo.springjpa.spirngbootjpa.repository.PersonRepository;
@@ -22,9 +25,47 @@ public class SpirngbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Person> persons = (List<Person>) repository.findAll(); 
-
-		persons.stream().forEach(person -> System.out.println(person));
+		// lista();
+		// oneFindxd();
+		create();
 	}
 
+
+	@Transactional
+	public void create(){
+
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.next();
+		String lastname = scanner.next();
+		String programingLanguage = scanner.next();
+		scanner.close();
+		Person person = new Person(null, name, lastname, programingLanguage);
+		var newPerson = repository.save(person);
+		System.out.println(newPerson);
+	}
+
+	@Transactional(readOnly = true)
+	private void lista(){
+		// List<Person> persons = (List<Person>) repository.findAll(); 
+
+		List<Person> persons = (List<Person>) repository.budcarByProgramingLanguage("Java"); 
+		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> persons2 = repository.obtenerPersonByProgramingLanguage("Java"); 
+		persons2.stream().forEach(person -> System.out.println(person[0]+" "+person[1]));
+	}
+
+	@Transactional(readOnly = true)
+	private void oneFindxd(){
+		// Person persona =  null;
+		// Optional<Person> optional = repository.findById(1L);
+		// if (optional.isPresent()) {
+		// 	persona = optional.get();
+		// }
+		// System.out.println(persona);	
+		repository.findById(2L).ifPresent(System.out::println);	
+		repository.leerPorNombre("Juan").ifPresent(System.out::println);
+		List<Person> pornombre = (List<Person>) repository.leerPorParecidoNombre("En");	
+		System.out.println(pornombre);
+	}
 }
--- src/main/java/com/pablo/springjpa/spirngbootjpa/repository/PersonRepository.java
+++ src/main/java/com/pablo/springjpa/spirngbootjpa/repository/PersonRepository.java
@@ -1,9 +1,25 @@
 package com.pablo.springjpa.spirngbootjpa.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pablo.springjpa.spirngbootjpa.entities.Person;
 
-public interface PersonRepository extends CrudRepository<Person, Long>{
-    
+public interface PersonRepository extends CrudRepository<Person, Long> {
+    List<Person> findByProgramingLanguage(String programingLanguage);
+
+    @Query("select p from Person p where p.programingLanguage = ?1")
+    List<Person> budcarByProgramingLanguage(String programingLanguage);
+
+    @Query("select p.name, p.lastname from Person p where p.programingLanguage = ?1")
+    List<Object[]> obtenerPersonByProgramingLanguage(String programingLanguage);
+
+    @Query("select p from Person p where p.name = ?1")
+    Optional<Person> leerPorNombre(String name);
+
+    @Query("select p from Person p where p.name like ?1%")
+    List<Person> leerPorParecidoNombre(String name);
 }
