50023e3eaf46f99b3e43b5d997280be1dab4ccb4
PabloSan1997 <jos_pab_997@hotmali.com>
2024-03-30 14:15:23 -0600

    Salida

--- src/main/java/com/pablin/springboot/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/pablin/springboot/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -1,12 +1,16 @@
 package com.pablin.springboot.springbootjparelationship;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.pablin.springboot.springbootjparelationship.entitites.Adress;
 import com.pablin.springboot.springbootjparelationship.entitites.Client;
 import com.pablin.springboot.springbootjparelationship.entitites.Invoice;
 import com.pablin.springboot.springbootjparelationship.repositories.ClientRespository;
@@ -28,27 +32,62 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// manyToOne();
-		manyToOneFindByIdClient();
+		// manyToOneFindByIdClient();
+		// oneToMany();
+		// oneToManyFind();
 	}
 
-	public void manyToOne(){
+
+	@Transactional
+	public void manyToOne() {
 		Client client = new Client("Pablo", "Santillana");
 		clientRespository.save(client);
-
-		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		Invoice invoice = new Invoice("Compras de Consolas", 2000L);
 		invoice.setClient(client);
 		var res = invoiceRepository.save(invoice);
 		System.out.println(res);
+		// Optional<Client> optionalcliente = clientRespository.findById(2L);
+		// optionalcliente.ifPresent(p -> {
+		// 	Invoice invoice = new Invoice("Compras de Consolas", 2000L);
+		// 	invoice.setClient(p);
+		// 	var res = invoiceRepository.save(invoice);
+		// 	System.out.println(res);
+		// });
 	}
-	public void manyToOneFindByIdClient(){
-		Optional<Client> clientOp = clientRespository.findById(1L);
-		if(clientOp.isPresent()){
+
+	@Transactional
+	public void manyToOneFindByIdClient() {
+		Optional<Client> clientOp = clientRespository.findById(2L);
+		if (clientOp.isPresent()) {
 			Client client = clientOp.orElseThrow();
 			System.out.println(client);
-			Invoice invoice =  new Invoice("Compra de consolas", 500L);
+			Invoice invoice = new Invoice("Compra de consolas", 500L);
 			invoice.setClient(client);
 			var res = invoiceRepository.save(invoice);
 			System.out.println(res);
 		}
+	}
+
+	@Transactional
+	public void oneToMany() {
+		Client client = new Client("Juana", "La Iguana");
+		Adress adress1 = new Adress("La condesa", 456);
+		Adress adress2 = new Adress("Reforma", 6565);
+		client.getAddreses().add(adress1);
+		client.getAddreses().add(adress2);
+		var res = clientRespository.save(client);
+		System.out.println(res);
+	}
+
+	@Transactional
+	public void oneToManyFind() {
+		Optional<Client> optionalcliente = clientRespository.findById(1L);
+		optionalcliente.ifPresent(p -> {
+			Adress adress1 = new Adress("Tlaxcala", 4854);
+			Adress adress2 = new Adress("Juarez", 65);
+			p.setAddreses(Arrays.asList(adress1, adress2));
+			var res = clientRespository.save(p);
+			System.out.println(res);
+		});
 	}
 }
--- src/main/java/com/pablin/springboot/springbootjparelationship/entitites/Client.java
+++ src/main/java/com/pablin/springboot/springbootjparelationship/entitites/Client.java
@@ -1,10 +1,18 @@
 package com.pablin.springboot.springbootjparelationship.entitites;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "clients")
@@ -16,42 +24,60 @@ public class Client {
     private String name;
     private String lastname;
 
-    public Client(){
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+        name = "tbl_clients_to_direcciones", 
+        joinColumns = @JoinColumn(name = "id_cliente"), 
+        inverseJoinColumns = @JoinColumn(name = "id_direcciones"),
+        uniqueConstraints = @UniqueConstraint(columnNames = {"id_direcciones"})
+        )
+    private List<Adress> addreses;
 
+    public Client() {
+        addreses = new ArrayList<Adress>();
     }
-
 
     public Client(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
+        this.addreses = new ArrayList<Adress>();
     }
-
-
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
+    public List<Adress> getAddreses() {
+        return addreses;
+    }
+
+    public void setAddreses(List<Adress> addreses) {
+        this.addreses = addreses;
+    }
 
     @Override
     public String toString() {
-        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + "}";
+        return " {id=" + id + ", name=" + name + ", lastname=" + lastname + ", addreses=" + addreses + "}";
     }
 
-    
 }
